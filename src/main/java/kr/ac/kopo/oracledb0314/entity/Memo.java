6f6ad366ef72fbf96d80629bd29f2902f18bcc53
SeokHoL <sukho8407@gmail.com>
2024-03-14 11:46:52 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/oracledb0314/entity/Memo.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.oracledb0314.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity //개체
+@Table(name ="tbl_memo") //테이블이름 지정
+@ToString // 보여주는 문자열
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+
+public class Memo {
+    @Id // 기본키 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //1씩 자동으로 증가
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+
+
+}
